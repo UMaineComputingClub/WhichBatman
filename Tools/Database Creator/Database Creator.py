f83633ceadf13ff32c8e2eb2c06be3d6315a2bc8
Christian Silva <christiansilvaprof@gmail.com>
2023-09-27 06:13:07 -0400

    added functionality which checks for the existence of the provided output folder and creates it if it does not exist

--- Tools/Database Creator/Database Creator.py
+++ Tools/Database Creator/Database Creator.py
@@ -5,6 +5,9 @@ import os
 def main():
     args = get_args_from_cmd_line()
     check_input_file_exists(args.input)
+    check_output_folder_exists(args.output)
+    title_index_map = {}
+
     exit()
 
 # This takes in command line arguments and passes them out as an object, args can be accessed by name i.e. args.input
@@ -19,10 +22,28 @@ def get_args_from_cmd_line():
 
 
 # This function checks for the existence of the input file, and terminates the program if it cannot be found
+# WARNING THIS ASSUMES CORRECT INPUT FILE FORMAT AND DOES NO CHECKING
 def check_input_file_exists(input_filepath):
     if not os.path.exists(input_filepath):
         print("The input file provided could not be found, terminating.")
         exit()
+
+
+def check_output_folder_exists(output_filepath):
+    if not os.path.exists(output_filepath):
+        print("The output folder provided could not be found.")
+        while True:
+            user_permission = input("Would you like to create a new folder to house output? (y/n): ")
+            if user_permission == 'y' or user_permission == "Y" or user_permission == "yes" or user_permission == "Yes":
+                print("Creating new folder at ", output_filepath)
+                os.mkdir(output_filepath)
+                break
+            elif user_permission == 'n' or user_permission == 'N' or user_permission == "no" or user_permission == "No":
+                print("With no output folder, program cannot continue, terminating")
+                exit()
+            else:
+                print("Invalid input, please provide response as yes or no")
+                continue
 
 
 if __name__ == "__main__":
